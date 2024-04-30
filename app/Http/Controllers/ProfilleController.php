d9bc2ef2dd2ff488a8c55610f1368a7b242f6a6a
JimyTavares <jimytavares@hotmail.com>
2024-04-30 07:49:43 -0300

    ajustes

--- app/Http/Controllers/ProfilleController.php
+++ app/Http/Controllers/ProfilleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -25,32 +27,6 @@ class ProfilleController extends Controller
             'user_exp' => $getUserData->exp,
             'user_classe' => $getUserData->classe,
         ];
-    }
-    
-    public function welcome(){
-        
-        $getUserData = $this->getUserData();
-        $dataAtual = date('Y-m-d');
-        
-        $countAssistindo = DB::table('assistindo')->count();
-        
-        $session_user = auth()->user();
-        $id_user = $session_user->id;
-        
-        //recupera todos os itens da coluna de uma tabela
-        $slc_assistEpisodios = assistindo::orderBy('id', 'asc')->pluck('episodio');
-        
-        $slc_assistindoAll = assistindo::orderBy('id', 'desc')->get();
-        
-        $slc_assistindoStop = assistindo::orderBy('updated_at', 'desc')->take(5)->get();
-        $slc_assistindo = assistindo::where('id_user', $id_user)
-            ->join('animes', 'animes.id', '=', 'assistindo.id_anime')
-            ->orderBy('animes.data_semana')
-            ->select('assistindo.*')
-            ->with(['nome_anime' => function ($query) {$query->orderBy('data_semana');}])
-            ->get();
-        
-        return view('welcome', compact(["getUserData", "slc_assistindo", "dataAtual", "slc_assistindoStop", "countAssistindo", "slc_assistEpisodios", "slc_assistindoAll"]));
     }
     
     public function plusAnime($id_anime, $id_assist){
@@ -89,24 +65,6 @@ class ProfilleController extends Controller
         $assistindoEp = $slc_assistindo->episodio;
         
         return response()->json(['newEP' => $assistindoEp]);
-    }
-    
-    public function formAnime(){
-        
-        $getUserData = $this->getUserData();
-        $DataAtual = date('Y');
-        
-        $slc_animeAll = animes::orderBy('id', 'desc')->get();
-        $table_animes = animes::whereJsonContains('genero', ["Fantasia"])->get();
-        
-        $anime = animes::all();
-        $campos = [];
-        
-        foreach ($anime as $animes) {
-            $campos[] = ['nome' => $animes->nome, 'id' => $animes->id];
-        }
-        
-        return view('pages.form_anime', compact(["DataAtual", "table_animes", "slc_animeAll", "getUserData", "campos"]));
     }
     
     public function formAnimePost(request $request){
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfilleController;
-
+use App\Http\Controllers\PageController;
 
 Route::middleware([ 'auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
     
     Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
     
-    Route::get('/', [ProfilleController::class, 'welcome'])->name('welcome');
+    Route::get('/', [PageController::class, 'welcome'])->name('welcome');
     Route::put('/api/plusanime/{id_anime}/{id_assist}', [ProfilleController::class, 'plusAnime'])->name('plusAnime');
     Route::put('/api/decreAmime/{id_anime}/{id_assist}', [ProfilleController::class, 'decreAnime'])->name('decreAnime');
     Route::put('/api/plusExp/{idUser}', [ProfilleController::class, 'plusExp'])->name('plusExp');
     Route::get('/api/create_parados/{id_assist}', [ProfilleController::class, 'createParados'])->name('createParados');
     
-    Route::get('/admin/formanime', [ProfilleController::class, 'formAnime'])->name('formAnime');
+    Route::get('/admin/formanime', [PageController::class, 'formAnime'])->name('formAnime');
     Route::post('/admin/formanime', [ProfilleController::class, 'formAnimePost'])->name('formAnimePost');
-//    Route::post('/animeAdd2', [ProfileController::class, 'animeAdd2'])->name('animeAdd2');
+    Route::get('/admin/list_anime', [PageController::class, 'list_anime'])->name('list_anime');
     
 });
